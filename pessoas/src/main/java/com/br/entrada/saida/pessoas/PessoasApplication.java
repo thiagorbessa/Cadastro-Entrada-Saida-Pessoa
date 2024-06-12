625b2eeac52ccc1f71c4113f1cc31f9332765499
thiagorbessa <43521019+thiagorbessa@users.noreply.github.com>
2024-06-12 14:31:27 -0300

    Atualizando versao do java no pom.xml

--- pessoas/src/main/java/com/br/entrada/saida/pessoas/PessoasApplication.java
+++ pessoas/src/main/java/com/br/entrada/saida/pessoas/PessoasApplication.java
@@ -3,6 +3,10 @@ package com.br.entrada.saida.pessoas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/",description = "Default server URL")}) // aponta para o contexto que o a aplicacao está rodando
 @SpringBootApplication
 public class PessoasApplication {
 
